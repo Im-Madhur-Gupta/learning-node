1fc9a68efeacc696b44f092f5f38a71d1956535b
Madhur Gupta <madhurgupta107@gmail.com>
2022-07-29 23:49:49 +0530

    code backup

--- Andrew Mead Course/task-manager-file-upload/src/index.js
+++ Andrew Mead Course/task-manager-file-upload/src/index.js
@@ -18,6 +18,19 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+const multer = require("multer");
+
+// configure multer for current use (can be done multiple times corresponding to multiple requests)
+// configuration would involve which type of file can be uploaded etc
+const upload = multer({
+  dest: "images", // name of the folder where files will be stored.
+});
+// endpoint for user to upload file
+app.post("/upload", upload.single("upload"), (req, res) => {
+  // IMP - What I give to upload.single("____") is the name of key corresponding to the file in the form data.
+  res.send();
+});
+
 app.listen(port, () => {
   console.log("server is running on port -", port);
 });
